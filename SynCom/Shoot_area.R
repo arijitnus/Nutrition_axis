af6146a5dd317e8383e85fc708b2cad70952941e
arijitnus <66009368+arijitnus@users.noreply.github.com>
2023-06-29 20:48:54 +0800

    Update Shoot_area.R

--- SynCom/Shoot_area.R
+++ SynCom/Shoot_area.R
@@ -2,25 +2,30 @@
 library(readxl)
 library(ggplot2)
 library(dplyr)
-LA<<-read_excel("/Users/arijitmukherjee/Downloads/SynCom_LA_Col.xlsx",sheet = "Sheet1",col_names = T,skip = 0)
+LA<<-read_excel("/Users/arijitmukherjee/Downloads/Shoot_area_combined.xlsx",sheet = "Sheet1",col_names = T,skip = 0)
 head(LA)
-LA$Sulphur<-as.factor(LA$Sulphur)
-LA$SynCom<-as.factor(LA$SynCom)
-LA$`Area (Total)`
+LA$Sulfur<-as.factor(LA$Sulfur)
+LA$microbiome<-as.factor(LA$microbiome)
+
+LA
+
+
 cols=c('#000000',"#DC143C")
 head(LA)
 level_order<-c("Heat killed","Active")
-level_order2<-c("Sufficient","Deficient")
-LA$Sulphur<-factor(LA$Sulphur,levels = level_order2)
+level_order2<-c("S1500","S15")
+LA$Sulfur<-factor(LA$Sulfur,levels = level_order2)
+LA$microbiome<-factor(LA$microbiome,levels = level_order)
+LA$Experiment<-as.factor(LA$Experiment)
 
-la<-ggplot(LA,aes(x=factor(SynCom,levels=level_order),y=`Area (Total)`))+
-  geom_boxplot(aes(col=Sulphur))+
-  geom_jitter(aes(col=Sulphur),size=2.5,width = 0.4, alpha=0.5)+
+la<-ggplot(LA,aes(x=factor(microbiome,levels = level_order),y=shoot_area))+
+  geom_boxplot(aes(col=Sulfur))+
+  geom_jitter(aes(col=Sulfur,shape=Experiment),size=2.5,width = 0.4, alpha=0.5)+
   scale_color_manual(values = cols)+
   scale_fill_manual(values = cols)+
   theme_classic()+
   xlab("")+
-  ylab(expression("Area in cm"^2))+
+  ylab(expression("Shoot area in cm"^2))+
   theme(axis.text.x = element_text(size = 14),
         axis.text.y = element_text(size = 14),
         axis.title.y = element_text(size = 14),
@@ -28,10 +33,11 @@ la<-ggplot(LA,aes(x=factor(SynCom,levels=level_order),y=`Area (Total)`))+
         axis.line = element_line(colour="black", size = 0.7),
         strip.background = element_blank())+
   scale_y_continuous(limits = c(0, 1), expand = expansion(mult = c(0, 0)))
-q<-la+facet_grid(.~Sulphur)
+q<-la+facet_grid(.~Sulfur)
 q
+
 ggsave(
-  "shoot_area_final.tiff",
+  "shoot_area_combined.tiff",
   plot = last_plot(),
   device = NULL,
   path = NULL,
@@ -42,6 +48,7 @@ ggsave(
   dpi = 400,
 )
 dev.off()
+
 
 
 
