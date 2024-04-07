1dcc9aae66d44c602f1a90b6e7f7b5463b17a1eb
arijitnus <66009368+arijitnus@users.noreply.github.com>
2024-04-07 09:46:50 +0800

    Update heatmap_ES_genotypes.R

--- Anion_analyses/heatmap_ES_genotypes.R
+++ Anion_analyses/heatmap_ES_genotypes.R
@@ -14,7 +14,7 @@ dat<-as.matrix(dat)
 col_fun<-colorRamp2(c(-1, 0, 1), c("blue", "white", "red"))
 hm<-Heatmap(dat, cluster_rows = F,cluster_columns = F,col = col_fun,rect_gp = gpar(col="black"),
         cell_fun = function(j, i, x, y, width, height, fill) {
-          grid.text(sprintf("%.1f", dat[i, j]), x, y, gp = gpar(fontsize = 24))
+          grid.text(sprintf("%.2f", dat[i, j]), x, y, gp = gpar(fontsize = 24))
         })
 hm
 
